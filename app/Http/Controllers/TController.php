c846c2563763066b7bbec4b70acfeefc127de7ae
abood arter <abd.alrhman.asem.bashir@gmail.com>
2023-11-03 18:56:29 +0300

    secound commit

---
+++ app/Http/Controllers/TController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\t;
+use App\Http\Requests\StoretRequest;
+use App\Http\Requests\UpdatetRequest;
+
+class TController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(t $t)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(t $t)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetRequest $request, t $t)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(t $t)
+    {
+        //
+    }
+}
